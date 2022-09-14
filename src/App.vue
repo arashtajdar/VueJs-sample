957347a230442bed3fca102fdd2782136961167c
a.tajdar@united-remote.com <arashtajdar@gmail.com>
2022-09-14 17:57:51 +0200

    Add new components to app and create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" style="width: 100px;" src="./assets/logo.png">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item" :class="{ active: current === 'HomePage' }">
+          <a class="nav-link" @click="current= 'HomePage'" href="#">Home</a>
+        </li>
+        <li class="nav-item" :class="{ active: current === 'AddCategory' }">
+          <a class="nav-link" @click="current= 'AddCategory'" href="#">Add Category</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <HomePage v-if="current==='HomePage'"/>
+  <AddCategory v-if="current==='AddCategory'"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AddCategory from './components/AddCategory.vue'
+import HomePage from './components/HomePage.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddCategory,HomePage
+  },
+  data(){
+    return {
+      current: "HomePage",
+    }
   }
 }
 </script>
@@ -23,4 +41,26 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.nav-item {
+  display: inline;
+  margin-right: 3%;
+  padding: 5px;
+  border: 1px solid #41b883;
+  background: white;
+}
+.nav-item.active{
+  background: #41b883;
+}
+.nav-item.active>.nav-link{
+  color:white;
+}
+.nav-link {
+  text-decoration: navajowhite;
+  color: black;
+  font-size: 16px;
+}
+.nav-link:hover {
+  color: #41b883;
+}
 </style>
+Footer
