92e7879253f49797e0cba0f458a8e01f5e897dc3
a.tajdar@united-remote.com <arashtajdar@gmail.com>
2022-09-14 17:44:07 +0200

    Install Basic alert module

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import * as Vue from "vue";
 import VueAxios from "vue-axios";
 import axios from "axios";
+import VueBasicAlert from 'vue-basic-alert'
 createApp(App).mount('#app')
 const app = Vue.createApp(App);
-app.use(VueAxios, axios);
+app.use(VueAxios, axios,VueBasicAlert);
