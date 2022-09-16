e69abe048b499db23668aea9079cd05a64e7a0b0
a.tajdar@united-remote.com <arashtajdar@gmail.com>
2022-09-16 19:22:47 +0200

    Refactor navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" style="width: 100px;" src="./assets/logo.png">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item" :class="{ active: current === 'HomePage' }">
