3cf2b6c6b542aa41d8484f498d45242f2503f0d8
a.tajdar@united-remote.com <arashtajdar@gmail.com>
2022-09-16 19:22:36 +0200

    Add bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import * as Vue from "vue";
 import VueAxios from "vue-axios";
 import axios from "axios";
 import VueBasicAlert from 'vue-basic-alert';
+import bootstrap from "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 createApp(App).mount('#app');
 const app = Vue.createApp(App);
-app.use(VueAxios, axios,VueBasicAlert);
+app.use(VueAxios, axios,VueBasicAlert,bootstrap);
